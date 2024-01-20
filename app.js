f147c75485bcda74279de3ac49f3acff54629bb0
Adınız Soyadınız <email@example.com>
2024-01-20 13:53:51 +0400

    selamm

--- app.js
+++ app.js
@@ -3,14 +3,44 @@ audio.src = './click.mp3';
 
 const cards =  document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let firstcard,secondCard;
 
-function FlipCard() {
+
+function flipCard() {
     this.classList.toggle('flip');
-}
 
+   if(!hasFlippedCard) {
+        hasFlippedCard = true
+        firstcard = this
 
-cards.forEach(card =>  card.addEventListener('click', ()=>{
+    }
+    else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        if (firstcard.dataset.framework === secondCard.dataset.framework){
+            setTimeout(() => {
+                firstcard.classList.toggle('remove');
+                secondCard.classList.toggle('remove');
+                firstcard.removeEventListener('click',flipCard);  
+                secondCard.removeEventListener('click',flipCard);  
+            }, 1500);
+            
+           
+        }
+
+        else {
+            setTimeout(() => {
+                firstcard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            } ,1500)
+        }
+    }
     
-    card.classList.toggle('flip')
 }
-));
+
+
+cards.forEach(card =>  card.addEventListener('click', flipCard));
+    
+
